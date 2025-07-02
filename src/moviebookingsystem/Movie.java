86a8551369a61dcb455ff2ab4f3f56f7f88ddfc7
Shubham Sinha <shubhamkumarsinha25@gmail.com>
2025-07-02 16:59:40 +0530

    movie object created

---
+++ src/moviebookingsystem/Movie.java
@@ -0,0 +1,26 @@
+package moviebookingsystem;
+
+public class Movie {
+    private int id;
+    private String name;
+    private int duration;
+    private String language;
+
+    public Movie(int id, String name, int duration) {
+        this.id = id;
+        this.name = name;
+        this.duration = duration;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+}
