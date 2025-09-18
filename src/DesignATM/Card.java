14d2c71ebc5a8fbc96f0beeca98f7d696cc5e7b8
Shubham Sinha <shubhamsinha@Shubhams-MacBook-Air.local>
2025-09-19 00:03:21 +0530

    card and Account class DTO is created

--- src/DesignATM/Card.java
+++ src/DesignATM/Card.java
@@ -1,5 +1,18 @@
 package DesignATM;
 
 public class Card {
+    private final String cardNumber;
+    private final  String pin;
+    public Card(String cardNumber, String pin){
+        this.cardNumber = cardNumber;
+        this.pin = pin;
+    }
 
+    public String getCardNumber() {
+        return cardNumber;
+    }
+
+    public String getPin() {
+        return pin;
+    }
 }
